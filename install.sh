f6c11f653302438a19e0d65bd2782663d8582c47
thabotitus <thabotitus@gmail.com>
2015-12-20 12:27:02 +0200

    install rvm

--- install.sh
+++ install.sh
@@ -1,21 +1,6 @@
 #! /bin/bash
 # A script to automeate the process of installing Rails and all it's dependencies.
-
-echo '=> Hi '$USER', beginning installation...'
-sleep 2
-echo '=> Installing dependencies...this may take awhile'
-
-function update_cache {
-	echo "-- Updating Cache..."
-	`sudo apt-get update &> log/install.log`
-	echo "-- Done updating cache..."
-}
-
-function install_dependencies {	
-	echo "-- Installing dependencies..."
-	`sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libqt4-dev &> log/install.log`
-	echo "-- Done installing dependencies..."
-}
+# V 0.0.1
 
 function operating_system_details {
 	OS=`uname -s`
@@ -37,7 +22,40 @@ function current_ruby_version {
 
 function current_rails_version {
 	RAILS_VERSION=`rails -v &> log/install.log`
+
+}
+function update_cache {
+	echo "-- Updating Cache..."
+	`sudo apt-get update &> log/install.log`
+	echo "-- Done updating cache..."
 }
 
-update_cache
-install_dependencies
+function install_dependencies {	
+	echo "-- Installing dependencies...this may take a while"
+	`sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libqt4-dev &> log/install.log`
+	echo "-- Done installing dependencies..."
+}
+
+function install_rvm {
+	echo "-- Installing RVM dependencies..."
+	`sudo apt-get -y install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev &> log/install.log`
+	echo "-- Installing RVM..."
+	`gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3`
+	`curl -L https://get.rvm.io | bash -s stable &> log/install.log`
+	`source ~/.rvm/scripts/rvm`
+	echo "-- Done installing RVM..."
+}
+
+function run_installation {
+	echo '=> Hi '$USER', beginning installation...'	
+	sleep 2
+	update_cache
+	install_dependencies
+	install_rvm
+}
+
+run_installation
+
+
+
+
